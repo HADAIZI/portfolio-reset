e7620334fb7cefbf5d9ba9d64a968527400118d1
HADAIZI <adamhadaizi2002@gmail.com>
2024-05-27 14:47:18 +0700

    Final I think

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -27,12 +27,12 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          IDK
-        </Link>
+      <Link
+  href="/"
+  className="text-2xl md:text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 hover:from-pink-500 hover:to-purple-500"
+>
+  Adam Haidar Azizi
+</Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button

58db590fd58f54bf284d698abea9040b3cf2fabd
HADAIZI <adamhadaizi2002@gmail.com>
2024-05-27 14:35:27 +0700

    coba aja

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import Image from "next/image";
 
 interface ProjectCardProps {
   imgUrl: string;
@@ -13,10 +14,14 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
-      <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
+      <div className="h-52 md:h-72 rounded-t-xl relative group">
+        <Image
+          src={imgUrl}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-xl"
+        />
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}

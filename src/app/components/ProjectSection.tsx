4c10662e44084583f603983a918b8508e9d16cba
Hadaizi <adamhadaizi2002@gmail.com>
2024-08-08 12:38:13 +0700

    nambahin

--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -87,6 +87,24 @@ const projectsData = [
     gitUrl: "https://github.com/HADAIZI/Project_dicoding",
     previewUrl: "Project Link",
   },
+  {
+    id: 10,
+    title: "Shinkaigan",
+    description: "Part of the Shinkaigan research project, this GAN model generates a anime picture from aa skech image based on Makoto Shinkai style using Unet architecture. The model is trained on a dataset of anime sketches and images to learn the mapping between the two domains.",
+    image: "/images/shinkaigan.jpeg",
+    tag: ["All", "Machine Learning"],
+    gitUrl: "https://github.com/echelon2718/ShinkaiGAN",
+    previewUrl: "Project Link",
+  },
+  {
+    id: 11,
+    title: "Batikhub",
+    description: "BatikHub is an application designed to classify various types of batik motifs based on user input. This application not only functions as a classification tool, but also as an educational tool regarding the history and meaning behind each identified batik motif. The results of the motif prediction will be stored in the history page, making it easier for users to access and learn about previously explored batik motifs. "
+    image: "/images/Batikhub.jpeg",
+    tag: ["All", "Machine Learning"],
+    gitUrl: "https://github.com/VanGarman21/BatikHub",
+    previewUrl: "Project Link",
+
 ];
 
 const ProjectsSection = () => {

62febe5139c99228c6548313bee4f7dcb987e469
Hadaizi <adamhadaizi2002@gmail.com>
2024-08-11 18:11:15 +0700

    ganti posisi

--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -71,24 +71,15 @@ const projectsData = [
   },
   {
     id: 8,
-    title: "Breast Cancer Prediction",
-    description: "Train a neural network on the Breast Cancer Dataset to predict if a tumor is malignant or benign. This involves data preprocessing, model training, and evaluation using accuracy and F1 Score metrics.",
-    image: "/images/Cancerpred.jpeg",
+    title: "Batikhub",
+    description: "BatikHub is an application designed to classify various types of batik motifs based on user input. This application not only functions as a classification tool, but also as an educational tool regarding the history and meaning behind each identified batik motif. The results of the motif prediction will be stored in the history page, making it easier for users to access and learn about previously explored batik motifs. ",
+    image: "/images/batikhub.png",
     tag: ["All", "Machine Learning"],
-    gitUrl: "https://github.com/HADAIZI/Breast-Cancer-Prediction",
-    previewUrl: "Collab Link",
-  },
-  {
-    id: 9,
-    title: "Bike Sharing Analysis",
-    description: "Analyzed the Bike Sharing Dataset using Python to uncover rental patterns and factors like weather, seasonal trends, and user behaviors. Presented insights through visualizations to support data-driven decisions for optimizing bike rental services.",
-    image: "/images/Bike-analysis.jpeg",
-    tag: ["All", "Machine Learning"],
-    gitUrl: "https://github.com/HADAIZI/Project_dicoding",
+    gitUrl: "https://github.com/VanGarman21/BatikHub",
     previewUrl: "Project Link",
   },
   {
-    id: 10,
+    id: 9,
     title: "Shinkaigan",
     description: "Part of the Shinkaigan research project, this GAN model generates a anime picture from aa skech image based on Makoto Shinkai style using Unet architecture. The model is trained on a dataset of anime sketches and images to learn the mapping between the two domains.",
     image: "/images/shinkaigan.png",
@@ -97,14 +88,25 @@ const projectsData = [
     previewUrl: "Project Link",
   },
   {
-    id: 11,
-    title: "Batikhub",
-    description: "BatikHub is an application designed to classify various types of batik motifs based on user input. This application not only functions as a classification tool, but also as an educational tool regarding the history and meaning behind each identified batik motif. The results of the motif prediction will be stored in the history page, making it easier for users to access and learn about previously explored batik motifs. ",
-    image: "/images/batikhub.png",
+    id: 10,
+    title: "Breast Cancer Prediction",
+    description: "Train a neural network on the Breast Cancer Dataset to predict if a tumor is malignant or benign. This involves data preprocessing, model training, and evaluation using accuracy and F1 Score metrics.",
+    image: "/images/Cancerpred.jpeg",
     tag: ["All", "Machine Learning"],
-    gitUrl: "https://github.com/VanGarman21/BatikHub",
+    gitUrl: "https://github.com/HADAIZI/Breast-Cancer-Prediction",
+    previewUrl: "Collab Link",
+  },
+  {
+    id: 11,
+    title: "Bike Sharing Analysis",
+    description: "Analyzed the Bike Sharing Dataset using Python to uncover rental patterns and factors like weather, seasonal trends, and user behaviors. Presented insights through visualizations to support data-driven decisions for optimizing bike rental services.",
+    image: "/images/Bike-analysis.jpeg",
+    tag: ["All", "Machine Learning"],
+    gitUrl: "https://github.com/HADAIZI/Project_dicoding",
     previewUrl: "Project Link",
   }
+  
+  
 
 ];
 
